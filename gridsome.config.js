befa24365696ca5eb00df74a79a623ddbd035454
Cleiton Pereira <cleiton.spereira@live.com>
2019-03-01 12:30:44 -0300

    ✨ use gridsome-plugin-typescript
    use gridsome-plugin-typescript to enable support to lang="ts"
    in script tag inside .vue Single File Components

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,19 +1,7 @@
 module.exports = {
   siteName: 'Gridsome',
   icon: 'src/assets/favicon.png',
-  plugins: [],
-  chainWebpack: (config) => {
-    config.module
-      .rule('typescript')
-        .test(/\.tsx?$/)
-        .use()
-          .loader('ts-loader')
-          .options({ appendTsSuffixTo: [/\.vue$/] });
-
-    config.resolve.extensions
-      .add('ts');
-
-    config.resolve.alias
-      .set('vue$', `${process.cwd()}/node_modules/vue/dist/vue.esm.js`);
-  },
+  plugins: [
+    { use: 'gridsome-plugin-typescript' },
+  ],
 }
