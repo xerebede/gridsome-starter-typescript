07b48913359a689cbee39d39c8da50bd32c4cef9
Cleiton Pereira <cleiton.spereira@live.com>
2019-06-29 14:00:47 -0300

    🚨 fix linter warnings

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -5,11 +5,24 @@
         <g-link to="/">{{ $static.metaData.siteName }}</g-link>
       </strong>
       <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about">About</g-link>
+        <g-link
+          class="nav__link"
+          to="/"
+        >
+          Home
+        </g-link>
+        <g-link
+          class="nav__link"
+          to="/about"
+        >
+          About
+        </g-link>
       </nav>
     </header>
-    <transition name="fade" appear>
+    <transition
+      name="fade"
+      appear
+    >
       <main>
         <slot />
       </main>
